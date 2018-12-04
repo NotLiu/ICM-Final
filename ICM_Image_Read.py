81d048b37ceedeb599d8fec54d24467fe604df2a
NotLiu <aliu@lediur.com>
2018-12-04 21:47:01 +0800

    Update ICM_Image_Read.py
    **Testing Method**

--- ICM_Image_Read.py
+++ ICM_Image_Read.py
@@ -5,4 +5,20 @@ img = Image.open('Andrew.jpg','r')
 
 pix_val = list(img.getdata())
 
+d = {}
+
+pix_sum = 0
+
+for i in pix_val:
+    for j in i:
+        if j in d:
+            d[j] += 1
+        else:
+            d[j] = 1
+
+for i in d:
+    pix_sum += d[i]
+
 print(pix_val)
+print(d)
+print('sum:',pix_sum/3)
