141f4c5fb8b4ef1fc0dab752a28468fec55d04ec
NotLiu <aliu@lediur.com>
2018-12-05 15:53:54 +0800

    Update ICM_Image_Read.py

--- ICM_Image_Read.py
+++ ICM_Image_Read.py
@@ -8,17 +8,63 @@ pix_val = list(img.getdata())
 d = {}
 
 pix_sum = 0
-
+'''
 for i in pix_val:
     for j in i:
         if j in d:
             d[j] += 1
         else:
             d[j] = 1
+'''
 
-for i in d:
-    pix_sum += d[i]
+d1 = {'R':{},'G':{},'B':{}}
+for i in pix_val: #(1,2,3)
+    index = 0
+    for j in d1: #(0:(),1:(),2:())
+        if i[index] in d1[j]:
+            d1[j][i[index]] += 1
+        else:
+            d1[j][i[index]] = 1
+        index += 1
 
-print(pix_val)
-print(d)
-print('sum:',pix_sum/3)
+for i in d1:
+    for j in i:
+        pix_sum += 1
+
+#R,G,B value sums
+
+r_sum = 0
+g_sum = 0
+b_sum = 0
+
+
+
+for i in d1:
+    for j in d1[i]:
+        if i == 'R':
+            r_sum += d1[i][j]*j
+        elif i == 'G':
+            g_sum += d1[i][j]*j
+        elif i == 'B':
+            b_sum += d1[i][j]*j
+
+
+print('All pixel RGB data:\n')
+print(pix_val,'\n')
+
+#print(d) #number of each number value among RGB
+
+print('Number of each R value:')
+for i in d1:
+    print(i,':',d1[i])
+
+print('Sum of Red values: ')
+print(r_sum)
+
+print('Sum of Green values: ')
+print(g_sum)
+
+print('Sum of Blue values: ')
+print(b_sum)
+
+print('Number of Pixels:',pix_sum//3)
